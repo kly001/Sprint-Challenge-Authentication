9e9c49251d3f439483d2a26fe44a97c975e557b9
kly001 <karenly1961@gmail.com>
2019-12-14 12:09:18 -0500

    Initialized Sprint

--- api/server.js
+++ api/server.js
@@ -15,4 +15,8 @@ server.use(express.json());
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', (req, res) => {
+    res.send("<h1> Let's get it on ! </h1>");
+  });
+
 module.exports = server;
