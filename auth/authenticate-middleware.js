fee85ae0a8b0ca5d85fc8300c2bf24a1bdb0d9a9
kly001 <karenly1961@gmail.com>
2019-12-14 12:29:23 -0500

    register; login

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,25 @@
   before granting access to the next middleware/route handler
 */
 
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
+// const Users = require('../users/users-model.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
-};
+
+  const token = req.headers.authorization;
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken ) => {
+      if(error) {
+        res.status(401).json({message:'This token has not been verified. You shall not pass...'})
+      } else {
+        req.decodedJwt = decodedToken;
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({message:'You do not have an authorized token. Go away !'})
+  }
+}
